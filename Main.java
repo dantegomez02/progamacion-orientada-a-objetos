64979ede5d1adf21a12638de34aec410495f1509
dantegomez02 <105761053+dantegomez02@users.noreply.github.com>
2026-04-02 11:16:56 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        Punto p1 = new Punto(1, 3);
-        Punto p2 = new Punto(4, 7);
+void main() {
 
-        System.out.println("Punto 1: " + p1.getX() + "@" + p1.getY());
-        double dist = p1.distancia(p2);
-        System.out.println("La distancia al punto 2 es: " + dist);
-    }
+
+
 }
